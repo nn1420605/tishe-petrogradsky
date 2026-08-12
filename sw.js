b9d07e2794d59b20b9fe1dafcdaaf7275faf3008
nn1420605 <315892993+nn1420605@users.noreply.github.com>
2026-08-13 00:21:28 +0300

    Prevent pedestrian routes across water

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='tishe-mobile-v7-tested-routes';
+const VERSION='tishe-mobile-v8-water-safe';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./map-content.js','./tile-meta.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(VERSION).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==VERSION).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
